80f7de2866f0b563fe5225c6188464191807f3ac
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-12-06 17:20:51 +0700

    Create string.go
    add string data type

---
+++ data type/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Mulzahrian")
+	fmt.Println("Ari")
+	fmt.Println("Rian")
+
+	fmt.Println(len("Mulzahrian"))
+	fmt.Println("Ari"[0])
+	fmt.Println("Rian"[2])
+}
