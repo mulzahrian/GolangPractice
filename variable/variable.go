08c19589ddd0780bdff0f0fc8ccedec7089f00e2
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-12-07 14:13:38 +0700

    Create variable.go
    add variable

---
+++ variable/variable.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Mulzahrian"
+	fmt.Println(name)
+
+	name = "Ari"
+	fmt.Println(name)
+
+	var friendName = "Rama"
+	fmt.Println(friendName)
+
+	var age = 22
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	country = "USA"
+	fmt.Println(country)
+
+}
