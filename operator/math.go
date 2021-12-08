5e6e3225040b10f6b25c89d0ac10b98ae5e87822
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-12-08 14:39:35 +0700

    Create math.go
    add math operator

---
+++ operator/math.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 10
+	var c = a + b
+
+	var result = 100 * 10
+
+	fmt.Println(result)
+
+	fmt.Println(c)
+	//U can use = + / * - %
+
+	//Augmented Assingments
+
+	var i = 10
+	i += 10 // i = i + 10
+
+	fmt.Println(i)
+
+	i++ // i = i + 1
+	fmt.Println(i)
+}
