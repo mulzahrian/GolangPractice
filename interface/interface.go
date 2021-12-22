57c6859afe61bad635a09627c3a2fb4501a168ed
Mulzahrian_One <72987552+mulzahrian@users.noreply.github.com>
2021-12-22 17:16:58 +0700

    Create interface.go
    add interface

---
+++ interface/interface.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type HasName interface {
+	GetName() string
+}
+
+func sayHello(hasName HasName) {
+	fmt.Println("Hello", hasName.GetName())
+}
+
+type Person struct {
+	Name string
+}
+
+func (person Person) GetName() string {
+	return person.Name
+}
+
+func main() { 
+	var rian Person
+	rian.Name = "Rian"
+
+	sayHello(rian)
+
+}
